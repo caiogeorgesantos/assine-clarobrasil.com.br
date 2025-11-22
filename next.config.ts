09d983129b8fa50289417ed6cbe117fbeb38b3ad
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-22 14:20:47 -0300

    feat: performance improvement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    cssChunking: true, // default
+  },
+
+  // 🖼️ Compressão e imagens otimizadas
+  images: {
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 dias
+  },
+
+  // 📦 Melhor cache estático no Vercel
+  assetPrefix: undefined,
 };
 
 export default nextConfig;
