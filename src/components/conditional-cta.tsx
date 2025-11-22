43d196d779a8c2a71580a9c16e22ca445e1856fe
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-22 16:07:10 -0300

    feat: update cta

--- src/components/conditional-cta.tsx
+++ src/components/conditional-cta.tsx
@@ -1,40 +1,50 @@
+"use client";
+
 import { getWhatsappLink } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import { useMemo } from "react";
 
 export function ConditionalCta() {
   const whatspAppLink = getWhatsappLink();
 
-  const now = new Date();
-  const hours = now.getHours();
+  const isAfter18 = useMemo(() => {
+    const now = new Date();
+    const hour = now.getHours();
+    return hour >= 18;
+  }, []);
 
-  if (hours < 18) {
-    return (
-      <Link
-        href={whatspAppLink}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="block w-full"
-      >
-        <button className="bg-whatsapp text-white text-shadow-sm text-center w-full py-2.5 px-6 font-medium rounded-full cursor-pointer flex flex-row items-center justify-center gap-2">
-          <Image
-            src="/icons/whatsapp.svg"
-            alt="WhatsApp"
-            width={24}
-            height={24}
-            className="w-6 h-6"
-          />
-          <span className="-mb-1">Compre pelo WhatsApp</span>
-        </button>
-      </Link>
-    );
+  if (isAfter18 === null) {
+    return null;
   }
 
   return (
-    <Link href={"#"} rel="noopener noreferrer" className="block w-full">
-      <button className="bg-hire text-[#1f1d1d] text-center w-full py-2.5 px-6 font-medium rounded-full cursor-pointer flex flex-row items-center justify-center">
-        <span className="-mb-1">Assine online</span>
-      </button>
-    </Link>
+    <>
+      {isAfter18 ? (
+        <Link href={"#"} rel="noopener noreferrer" className="block w-full">
+          <button className="bg-hire text-[#1f1d1d] text-center w-full py-2.5 px-6 font-medium rounded-full cursor-pointer flex flex-row items-center justify-center">
+            <span className="-mb-1">Assine online</span>
+          </button>
+        </Link>
+      ) : (
+        <Link
+          href={whatspAppLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="block w-full"
+        >
+          <button className="bg-whatsapp text-white text-shadow-sm text-center w-full py-2.5 px-6 font-medium rounded-full cursor-pointer flex flex-row items-center justify-center gap-2">
+            <Image
+              src="/icons/whatsapp.svg"
+              alt="WhatsApp"
+              width={24}
+              height={24}
+              className="w-6 h-6"
+            />
+            <span className="-mb-1">Compre pelo WhatsApp</span>
+          </button>
+        </Link>
+      )}
+    </>
   );
 }
