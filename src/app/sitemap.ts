480d8af50c528da7c8f07d2ad058f0f6eddf497b
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-22 16:17:44 -0300

    feat: update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,25 @@
-import { SITE_URL } from '@/constants'
-import type { MetadataRoute } from 'next'
+import { SITE_URL } from '@/constants';
+import menuParaEmpresas from "@/data/para-empresas/menu.json";
+import menuParaVc from "@/data/para-vc/menu.json";
+import type { MetadataRoute } from 'next';
+
+const urlsFromMenu = [
+  [...menuParaVc, ...menuParaEmpresas].filter(item => item.url !== "/").map(item => ({
+    url: `${SITE_URL}${item.url}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly' as const,
+    priority: 0.8,
+  })),
+]
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: SITE_URL,
+      url: `${SITE_URL}/`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
     },
+    ...urlsFromMenu.flat(),
   ]
 }
