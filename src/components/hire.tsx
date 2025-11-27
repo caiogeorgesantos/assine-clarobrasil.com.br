bf746dcf8fd378848901dca7fc0e283c7db791db
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-27 00:14:31 -0300

    feat: render proper card following the pathname

--- src/components/hire.tsx
+++ src/components/hire.tsx
@@ -1,6 +1,8 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { getWhatsappLink } from "@/lib/utils";
+import { getContextFromPathname, getWhatsappLink } from "@/lib/utils";
 import {
   Building,
   Cloud,
@@ -13,8 +15,11 @@ import {
   Wifi,
 } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Hire() {
+  const pathname = usePathname();
+  const { isParaVoce, isParaEmpresas } = getContextFromPathname(pathname);
   const whatsappLink = getWhatsappLink();
 
   const residentialServices = [
@@ -63,6 +68,102 @@ export function Hire() {
     },
   ];
 
+  function renderResidential() {
+    return (
+      <div className="bg-white rounded-xl p-8 shadow-sm w-full">
+        <div className="flex items-center mb-8">
+          <Home className="w-8 h-8 text-[#E60000] mr-3 stroke-2" />
+          <h3 className="text-3xl font-bold text-[#E60000] uppercase">
+            Residencial
+          </h3>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          {residentialServices.map((service, index) => (
+            <Card
+              key={index}
+              className="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300"
+            >
+              <CardContent className="py-1 px-5 min-h-[110px]">
+                <div className="flex items-start space-x-4">
+                  <div className="flex-shrink-0 text-[#E60000] mt-1">
+                    {service.icon}
+                  </div>
+                  <div className="flex-grow">
+                    <p className="text-gray-800 mb-4 leading-relaxed">
+                      <span className="font-bold">{service.title}</span>{" "}
+                      {service.description}
+                    </p>
+                    <Link
+                      href={whatsappLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button
+                        size="sm"
+                        className="bg-black hover:bg-gray-800 text-white px-6 py-2 rounded-lg transition-colors duration-200"
+                      >
+                        Saber mais
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  function renderBusiness() {
+    return (
+      <div className="bg-white rounded-xl p-8 shadow-sm w-full">
+        <div className="flex items-center mb-8">
+          <Building className="w-8 h-8 text-[#E60000] mr-3 stroke-2" />
+          <h3 className="text-3xl font-bold text-[#E60000] uppercase">
+            Empresarial
+          </h3>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+          {businessServices.map((service, index) => (
+            <Card
+              key={index}
+              className="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300"
+            >
+              <CardContent className="py-1 px-5 min-h-[110px]">
+                <div className="flex items-start space-x-4">
+                  <div className="flex-shrink-0 text-[#E60000] mt-1">
+                    {service.icon}
+                  </div>
+                  <div className="flex-grow">
+                    <p className="text-gray-800 mb-4 leading-relaxed">
+                      <span className="font-bold">{service.title}</span>{" "}
+                      {service.description}
+                    </p>
+                    <Link
+                      href={whatsappLink}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Button
+                        size="sm"
+                        className="bg-black hover:bg-gray-800 text-white px-6 py-2 rounded-lg transition-colors duration-200"
+                      >
+                        Saber mais
+                      </Button>
+                    </Link>
+                  </div>
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   return (
     <section className="py-16 bg-[#F7F7F7]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -76,96 +177,12 @@ export function Hire() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
+        <div className="grid grid-cols-1 gap-16">
           {/* Residencial */}
-          <div className="bg-white rounded-xl p-8 shadow-sm">
-            <div className="flex items-center mb-8">
-              <Home className="w-8 h-8 text-[#E60000] mr-3 stroke-2" />
-              <h3 className="text-3xl font-bold text-[#E60000] uppercase">
-                Residencial
-              </h3>
-            </div>
-
-            <div className="space-y-5">
-              {residentialServices.map((service, index) => (
-                <Card
-                  key={index}
-                  className="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300"
-                >
-                  <CardContent className="py-1 px-5 min-h-[110px]">
-                    <div className="flex items-start space-x-4">
-                      <div className="flex-shrink-0 text-[#E60000] mt-1">
-                        {service.icon}
-                      </div>
-                      <div className="flex-grow">
-                        <p className="text-gray-800 mb-4 leading-relaxed">
-                          <span className="font-bold">{service.title}</span>{" "}
-                          {service.description}
-                        </p>
-                        <Link
-                          href={whatsappLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <Button
-                            size="sm"
-                            className="bg-black hover:bg-gray-800 text-white px-6 py-2 rounded-lg transition-colors duration-200"
-                          >
-                            Saber mais
-                          </Button>
-                        </Link>
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              ))}
-            </div>
-          </div>
+          {isParaVoce && renderResidential()}
 
           {/* Empresarial */}
-          <div className="bg-white rounded-xl p-8 shadow-sm">
-            <div className="flex items-center mb-8">
-              <Building className="w-8 h-8 text-[#E60000] mr-3 stroke-2" />
-              <h3 className="text-3xl font-bold text-[#E60000] uppercase">
-                Empresarial
-              </h3>
-            </div>
-
-            <div className="space-y-5">
-              {businessServices.map((service, index) => (
-                <Card
-                  key={index}
-                  className="bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md hover:bg-gray-50 transition-all duration-300"
-                >
-                  <CardContent className="py-1 px-5 min-h-[110px]">
-                    <div className="flex items-start space-x-4">
-                      <div className="flex-shrink-0 text-[#E60000] mt-1">
-                        {service.icon}
-                      </div>
-                      <div className="flex-grow">
-                        <p className="text-gray-800 mb-4 leading-relaxed">
-                          <span className="font-bold">{service.title}</span>{" "}
-                          {service.description}
-                        </p>
-                        <Link
-                          href={whatsappLink}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          <Button
-                            size="sm"
-                            className="bg-black hover:bg-gray-800 text-white px-6 py-2 rounded-lg transition-colors duration-200"
-                          >
-                            Saber mais
-                          </Button>
-                        </Link>
-                      </div>
-                    </div>
-                  </CardContent>
-                </Card>
-              ))}
-            </div>
-          </div>
+          {isParaEmpresas && renderBusiness()}
         </div>
       </div>
     </section>
