5fd3e71973832ff24217473eb12b0cfeb97112bd
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-11-28 01:03:37 -0300

    feat: update header height

--- src/components/header-conditional-cta.tsx
+++ src/components/header-conditional-cta.tsx
@@ -3,13 +3,24 @@
 import { AssineCta } from "@/components/common/assine-cta";
 import { WhatsappCta } from "@/components/common/whatsapp-cta";
 import { isWorkingHours } from "@/lib/utils";
+import { useEffect, useState } from "react";
 
 export function HeaderConditionalCta() {
-  const workingHours = isWorkingHours();
+  const [showWhatsAppCta, setShowWhatsAppCta] = useState<boolean | null>(null);
 
-  if (workingHours) {
+  useEffect(() => {
+    const workingHours = isWorkingHours();
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setShowWhatsAppCta(workingHours);
+  }, []);
+
+  if (showWhatsAppCta) {
     return <WhatsappCta />;
   }
 
-  return <AssineCta />;
+  if (showWhatsAppCta === false) {
+    return <AssineCta />;
+  }
+
+  return <div className="w-32 h-10" />;
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,8 +13,8 @@ export function Header({ pathname }: { pathname: string }) {
 
   return (
     <header>
-      <div className="bg-black-level-1 text-white">
-        <div className="container mx-auto py-4 px-2 lg:px-0 flex flex-row gap-8 lg:gap-4 items-center lg:justify-between">
+      <div className="bg-black-level-1 text-white min-h-[76px]">
+        <div className="container mx-auto py-4 px-2 lg:px-0 flex flex-row gap-8 lg:gap-4 items-center lg:justify-between h-full">
           <div className="relative w-full lg:w-auto flex flex-row-reverse items-center justify-end gap-1">
             <Link href="/">
               <h1>
@@ -50,7 +50,7 @@ export function Header({ pathname }: { pathname: string }) {
           </div>
 
           {/* Fale pelo WhatsApp */}
-          <div className="flex-1 lg:flex-0">
+          <div className="flex-1 lg:flex-0 max-h-10">
             <HeaderConditionalCta />
           </div>
         </div>
